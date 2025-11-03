dcaacd1546cef613dc8cb471cca0d5cc0f58040b
GN15H <cristobalochoacadavid@gmail.com>
2025-11-02 20:57:42 -0500

    added auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { UserTypesModule } from './user_types/user_types.module';
+// import { UsersModule } from './users/users.module';
+// import { UserTypesModule } from './user_types/user_types.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
-  imports: [UsersModule, UserTypesModule],
+  imports: [/*UsersModule, UserTypesModule, */AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,38 +1,55 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { User } from './entities/user.entity';
+// import { PrismaService } from 'src/prisma/prisma.service';
+
+export type User = any;
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) { }
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  constructor(/*private prisma: PrismaService*/) { }
 
   async create(createUserDto: CreateUserDto) {
     // const userData = new User(createUserDto.)
-    await this.prisma.users.create({
-      data: {
-        dni: createUserDto.dni,
-        name: createUserDto.name,
-        last_name: createUserDto.lastName,
-        email: createUserDto.email,
-        password: createUserDto.password,
-        birth_date: createUserDto.birthDate,
-        user_type_id: 1
-      }
-    });
+    // await this.prisma.users.create({
+    //   data: {
+    //     dni: createUserDto.dni,
+    //     name: createUserDto.name,
+    //     last_name: createUserDto.lastName,
+    //     email: createUserDto.email,
+    //     password: createUserDto.password,
+    //     birth_date: createUserDto.birthDate,
+    //     user_type_id: 1
+    //   }
+    // });
 
     return 'This action adds a new user';
   }
 
   async findAll() {
-    return await this.prisma.users.findMany();
-    // return 'huh';
+    // return await this.prisma.users.findMany();
+    return 'huh';
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
   }
+  // findOne(id: number) {
+  //   return `This action returns a #${id} user`;
+  // }
 
   update(id: number, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
